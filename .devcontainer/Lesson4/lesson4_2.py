8fa04db3b3130845da3f42d793e609902f884aba
WeiYC <weiyu1213@gmail.com>
2024-11-23 03:05:02 +0000

    1123_04

--- .devcontainer/Lesson4/lesson4_2.py
+++ .devcontainer/Lesson4/lesson4_2.py
@@ -42,15 +42,19 @@ def new_func():
             print(f"您輸入的體重:{weight}(Kg)及身高:{height}(cm),\n計算後取得的BMI值約為{bmi:.2f},\n您的BMI被歸類為{status}。")
             break
         
-#初始計算次數       
-counter = 0
-while(True):
-    new_func()
-    #每計算過一次加一
-    counter +=1
-    print(f"=======以上為您第{counter}次輸入後計算的BMI資料=======")
+def main():
+    #初始計算次數       
+    counter = 0
+    while(True):
+        new_func()
+        #每計算過一次加一
+        counter +=1
+        print(f"=======以上為您第{counter}次輸入後計算的BMI資料=======")
+        
+        responed = input("請問您還要再計算其他BMI嗎？(y,n) ").lower().strip() #自動將人員輸入空格刪除及轉換成小寫
+        if responed != "y":
+            break
+    print(f"程式結束。總共計算了 {counter} 次 BMI。")
     
-    responed = input("請問您還要再計算其他BMI嗎？(y,n) ").lower().strip() #自動將人員輸入空格刪除及轉換成小寫
-    if responed != "y":
-        break
-print(f"程式結束。總共計算了 {counter} 次 BMI。")
+if __name__=='__main__':
+    main()
