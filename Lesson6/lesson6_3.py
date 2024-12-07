31c308b90c5acf483d91d04bd908670eb9612019
WeiYC <weiyu1213@gmail.com>
2024-12-07 07:54:35 +0000

    1207_11

--- Lesson6/lesson6_3.py
+++ Lesson6/lesson6_3.py
@@ -3,7 +3,16 @@ import tools
 
 
 sitenames:list[str] = tools.get_sitename(excel_name='aqi.xlxs')
-add_selectbox = st.sidebar.selectbox(
-    "請選擇站點名稱",
+#add_selectbox = st.sidebar.selectbox(
+#    "請選擇站點名稱",
+#    sitenames
+#)
+# With Notation
+
+with st.sidebar: #with建立於文件變數底下ㄌ
+    add_selectbox = st.selectbox(
+    "請選擇站點名稱:",
     sitenames
-)
+    )
+    
+    st.title(f"{add_selectbox}")
