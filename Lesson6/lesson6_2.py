afbc8a7cc0de74bd85e00d6cca59ce30cfbf46d4
WeiYC <weiyu1213@gmail.com>
2024-12-07 14:56:08 +0000

    1207_15

--- Lesson6/lesson6_2.py
+++ Lesson6/lesson6_2.py
@@ -1,18 +1,19 @@
-import tools #將模組化方法匯入
+import tools  # 將模組化方法匯入
 import openpyxl
-from openpyxl import Workbook,worksheet
+from openpyxl import Workbook, worksheet
+
 
 def main():
-    sitenames:list[str] = tools.get_sitename(excel_name='aqi.xlsx')
+    sitenames: list[str] = tools.get_sitename(excel_name='aqi.xlsx')
     print(sitenames)
-    wb:Workbook = openpyxl.Workbook()
-    sheet:worksheet = wb.active
+    wb: Workbook = openpyxl.Workbook()
+    sheet: worksheet = wb.active
     sheet.title = "站點名稱"
-    for idy, name in enumerate(sitenames): #enumerate ?
+    for idy, name in enumerate(sitenames):  # enumerate ?
         print(name)
-        sheet.cell(column=1,row =idy+1,value=name)
+        sheet.cell(column=1, row=idy+1, value=name)
     wb.save('老闆要的資料.xlsx')
-    
-    
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     main()
--- Lesson6/lesson6_3.py
+++ Lesson6/lesson6_3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tools
 
 
-sitenames:list[str] = tools.get_sitename(excel_name='aqi.xlxs')
+sitenames: list[str] = tools.get_sitename(excel_name='aqi.xlxs')
 add_selectbox = st.sidebar.selectbox(
     "請選擇站點名稱",
     sitenames
