445e9e880e3c17bdd08d4d6c843a2231177b48fb
WeiYC <weiyu1213@gmail.com>
2024-12-07 08:22:56 +0000

    1207_13

--- Lesson6/lesson6_3.py
+++ Lesson6/lesson6_3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tools
 
 
-sitenames:list[str] = tools.get_sitename(excel_name='aqi.xlxs')
+sitenames:list[str] = tools.get_sitename(excel_name='aqi.xlsx')
 #add_selectbox = st.sidebar.selectbox(
 #    "請選擇站點名稱",
 #    sitenames
@@ -18,11 +18,12 @@ with st.sidebar: #with建立於文件變數底下ㄌ
     st.title(f"{add_selectbox}")
     
 allData:list[dict] = tools.get_aqi(excel_name='aqi.xlsx')
+#寫法一
 #selected_item:list[dict] = []
 #for item in allData:
 #    if item['sitename'] == add_selectbox:
 #        selected_item.append(item)
 
-
+#寫法二
 select_item:list[dict] = [ item for item in allData if item['sitename']==add_selectbox]
 st.table(data = select_item)
--- Lesson6/tools.py
+++ Lesson6/tools.py
@@ -1,26 +1,28 @@
 from openpyxl import load_workbook, workbook, worksheet
 from pprint import pprint
-#讀入資料的function
-def get_aqi(excel_name:str)-> list[dict]:
-    wb:workbook = load_workbook(excel_name)
-    sheet:worksheet = wb.worksheets[0]
+# 讀入資料的function
+
+
+def get_aqi(excel_name: str) -> list[dict]:
+    wb: workbook = load_workbook(excel_name)
+    sheet: worksheet = wb.worksheets[0]
     type(sheet)
-    sheets:list = []
+    sheets: list = []
     # comprehensions語法快速建立list
     column_name = [cell.value for cell in list(sheet)[0]]
     print(column_name)
 
     for row in list(sheet)[1:]:
-        site:dict = {column_name[idx]:cell.value for idx, cell in enumerate(row)}
+        site: dict = {column_name[idx]                      : cell.value for idx, cell in enumerate(row)}
         sheets.append(site)
     return sheets
 
-def get_sitename(excel_name:str)->list[str]:
-    data:list[dict] = get_aqi(excel_name='aqi.xlsx')
-    sitenames:list =[]
+
+def get_sitename(excel_name: str) -> list[str]:
+    data: list[dict] = get_aqi(excel_name='aqi.xlsx')
+    sitenames: list = []
     for item in data:
         sitenames.append(item['sitename'])
-        #刪除重複的
+        # 刪除重複的
     sitenames = list(set(sitenames))
     return sitenames
-    
