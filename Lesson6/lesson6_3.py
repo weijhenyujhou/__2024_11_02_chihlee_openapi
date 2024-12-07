fb7e8bde85eb0e2225c4e611895896e11df26b4e
WeiYC <weiyu1213@gmail.com>
2024-12-07 15:09:38 +0000

    1207_18

--- Lesson6/lesson6_3.py
+++ Lesson6/lesson6_3.py
@@ -7,3 +7,15 @@ add_selectbox = st.sidebar.selectbox(
     "請選擇站點名稱",
     sitenames
 )
+
+allData: list[dict] = tools.get_aqi(excel_name='aqi.xlsx')
+# 寫法一
+# selected_item:list[dict] = []
+# for item in allData:
+#    if item['sitename'] == add_selectbox:
+#        selected_item.append(item)
+
+# 寫法二
+select_item: list[dict] = [
+    item for item in allData if item['sitename'] == add_selectbox]
+st.table(data=select_item)
