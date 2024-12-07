9206f445b4bdb96b38e1a2aaf7d0a1ce3e056349
WeiYC <weiyu1213@gmail.com>
2024-12-07 06:25:52 +0000

    1207_07

--- Lesson6/lesson6_2.py
+++ Lesson6/lesson6_2.py
@@ -1,23 +1,15 @@
-from openpyxl import load_workbook, workbook, worksheet
-from pprint import pprint
-#讀入資料的function
-def get_aqi(excel_name:str)-> list[dict]:
-    wb:workbook = load_workbook(excel_name)
-    sheet:worksheet = wb.worksheets[0]
-    type(sheet)
-    sheets:list = []
-    # comprehensions語法快數建立list
-    column_name = [cell.value for cell in list(sheet)[0]]
-    print(column_name)
-
-    for row in list(sheet)[1:]:
-        site:dict = {column_name[idx]:cell.value for idx, cell in enumerate(row)}
-        sheets.append(site)
-    return sheets
+import tools
 
 def main():
-    data:list[dict] = get_aqi(excel_name='aqi.xlsx')
-    pprint(data)
+    data:list[dict] = tools.get_aqi(excel_name='aqi.xlsx')
+    sitenames:list =[]
+    for item in data:
+        sitenames.append(item['sitename'])
+        #刪除重複的
+        sitenames = list(set(sitenames))
+        for name in sitenames:
+            print(name)
+    print(len(sitenames))
 
 if __name__=='__main__':
     main()
