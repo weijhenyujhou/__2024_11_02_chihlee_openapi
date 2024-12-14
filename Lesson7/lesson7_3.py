375c5ac67e0b9cc161e789a0644852deaf9840ee
WeiYC <weiyu1213@gmail.com>
2024-12-14 06:44:10 +0000

    1214_08_MB

--- Lesson7/lesson7_3.py
+++ Lesson7/lesson7_3.py
@@ -1,7 +1,7 @@
-import tools
+from tools import taipei
 
 try:
-    youBike_data:list[dict] = tools.get_youBikes()
+    youBike_data:list[dict] = taipei.get_youBikes()
 except Exception as e: #修改當連線發生異常時,tools內的異常訊息可以傳遞出來
     print(e)
 else:
