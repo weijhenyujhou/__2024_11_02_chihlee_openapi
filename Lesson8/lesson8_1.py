438d0dc50136cf4cffc14cfc5e93089ac8d26ee4
WeiYC <weiyu1213@gmail.com>
2024-12-21 02:31:38 +0000

    1221_03_MB

--- Lesson8/lesson8_1.py
+++ Lesson8/lesson8_1.py
@@ -2,10 +2,12 @@ import streamlit as st
 #手動建立session_state, 並且設定初始值為0 
 if "counter" not in st.session_state:
     #st.session_state["counter"] = 0
-    st.session_state.counter = 0
+    st.session_state.counter = 1
 
-st.session_state.counter += 1
+def increment_counter(args):
+    st.session_state.counter += args[0]
+    st.session_state.counter -= args[1]
 
 st.header(f"這頁已經執行 {st.session_state.counter} 次.")
-st.button("再執行一次", key="restart") # key="restar"不管有沒有建立都會自動建立
+st.button("再執行一次", key="restart",help='按下時要加1',on_click=increment_counter,args=(5,3)) # key="restar"不管有沒有建立都會自動建立
 st.write(st.session_state)
---
+++ Lesson8/lesson8_2.py
@@ -0,0 +1,13 @@
+import streamlit as st
+#手動建立session_state, 並且設定初始值為0 
+if "counter" not in st.session_state:
+    #st.session_state["counter"] = 0
+    st.session_state.counter = 1
+
+def increment_counter(**kwargs):
+    st.session_state.counter += kwargs['first']
+    st.session_state.counter -= kwargs['second']
+
+st.header(f"這頁已經執行 {st.session_state.counter} 次.")
+st.button("再執行一次", key="restart",help='按下時要加1',on_click=increment_counter,kwargs={'first':5,'second':3}) # key="restar"不管有沒有建立都會自動建立
+st.write(st.session_state)
