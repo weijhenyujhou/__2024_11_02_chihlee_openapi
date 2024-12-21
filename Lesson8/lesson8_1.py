fa4dcb7e18136da445066670343301c38265fb76
WeiYC <weiyu1213@gmail.com>
2024-12-21 02:07:15 +0000

    1221_02_MB

--- Lesson8/lesson8_1.py
+++ Lesson8/lesson8_1.py
@@ -7,5 +7,5 @@ if "counter" not in st.session_state:
 st.session_state.counter += 1
 
 st.header(f"這頁已經執行 {st.session_state.counter} 次.")
-st.button("再執行一次", key="restar") # key="restar"不管有沒有建立都會自動建立
+st.button("再執行一次", key="restart") # key="restar"不管有沒有建立都會自動建立
 st.write(st.session_state)
